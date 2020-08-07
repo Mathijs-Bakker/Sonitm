4a98ac70b5763e385aa7a5272f2d2fe1821c66ac
Mathijs Bakker <mathijs_bakker@me.com>
2020-08-08 00:38:16 +0200

    Wip - Friends

--- Net/SocialLibrary/FriendsTests/FriendCollection.cs
+++ Net/SocialLibrary/FriendsTests/FriendCollection.cs
@@ -14,12 +14,21 @@ namespace sutsTests
         }
         
         [Test]
-        public void Test()
+        public void Sut_Should_Have_a_Name_Poperty_OfType_String()
         {
-            
+            var sut = new Friend("a");
+            sut.Name.Should().BeOfType<string>();
+            sut.Name.Should().Be("a");
         }
     }
 
     public struct Friend
-    {}
+    {
+        public string Name { get; private set; }
+
+        public Friend (string name)
+        {
+            Name = name;
+        }
+    }
 }
