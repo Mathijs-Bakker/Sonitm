6b8b7e9661365e938ef8c6850108c693e9a094ad
Mathijs Bakker <mathijs_bakker@me.com>
2020-08-08 09:11:44 +0200

    Rename FriendTests class - fix namespace - append Id property

--- Net/SocialLibrary/FriendsTests/FriendTests.cs
+++ Net/SocialLibrary/FriendsTests/FriendTests.cs
@@ -1,10 +1,10 @@
 using NUnit.Framework;
 using FluentAssertions;
 
-namespace sutsTests
+namespace FriendsTests
 {
     [TestFixture]
-    public class sutCollection
+    public class FriendTests
     {
         [Test]
         public void Sut_Is_TypeOf_sut()
@@ -16,18 +16,28 @@ namespace sutsTests
         [Test]
         public void Sut_Should_Have_a_Name_Poperty_OfType_String()
         {
-            var sut = new Friend("a");
+            var sut = new Friend(0, "a");
             sut.Name.Should().BeOfType<string>();
             sut.Name.Should().Be("a");
+        }
+
+        [Test]
+        public void Sut_Should_Have_an_Id_Property_OfType_Int()
+        {
+            var sut = new Friend(1, "");
+            sut.Id.Should().BeOfType(typeof(int));
+            sut.Id.Should().Be(1);
         }
     }
 
     public struct Friend
     {
+        public int Id { get; private set; }
         public string Name { get; private set; }
 
-        public Friend (string name)
+        public Friend (int id, string name)
         {
+            Id = id;
             Name = name;
         }
     }
