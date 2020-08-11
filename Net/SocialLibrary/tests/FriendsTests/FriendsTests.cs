cece599ebbac3f4ebd627d5c2f941491e17c0b3b
Mathijs Bakker <mathijs_bakker@me.com>
2020-08-11 06:04:35 +0200

    Add FriendBuilder test helper class

--- Net/SocialLibrary/tests/FriendsTests/FriendsTests.cs
+++ Net/SocialLibrary/tests/FriendsTests/FriendsTests.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using FluentAssertions;
 using Social.Friends;
+using System;
 
 namespace FriendsTests
 {
@@ -26,7 +27,59 @@ namespace FriendsTests
         public void Update_Should_()
         {
             var sut = new Friends();
-            sut.List.Add(new Friend(0, "a"));
+            sut.List.Add(
+                new FriendBuilder()
+                    .WithId(1)
+                    .WithName("a")
+                    .WithOnlineStatus(false)
+                    .WithLastSeen(new DateTime(2008, 12, 2))
+                    .WithLevel(1)
+                    .Build()
+                );
+        }
+    }
+
+    public class FriendBuilder
+    {
+        int _id;
+        string _name;
+        bool _isOnline;
+        DateTime _lastSeen;
+        int _level;
+
+        public FriendBuilder WithId(int id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public FriendBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public FriendBuilder WithOnlineStatus(bool isOnline)
+        {
+            _isOnline = isOnline;
+            return this;
+        }
+
+        public FriendBuilder WithLastSeen(DateTime lastSeen)
+        {
+            _lastSeen = lastSeen;
+            return this;
+        }
+
+        public FriendBuilder WithLevel(int level)
+        {
+            _level = level;
+            return this;
+        }
+
+        public Friend Build()
+        {
+            return new Friend(_id, _name, _isOnline, _lastSeen, _level);
         }
     }
 }
