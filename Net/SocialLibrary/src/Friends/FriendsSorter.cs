7421ac59200f9bfc521d61353244eec0026f93d0
Mathijs Bakker <mathijs_bakker@me.com>
2020-08-18 17:07:05 +0200

    Refactor

--- Net/SocialLibrary/src/Friends/FriendsSorter.cs
+++ Net/SocialLibrary/src/Friends/FriendsSorter.cs
@@ -5,24 +5,22 @@ namespace Social.Friends
 {
     public static class OrderFriends
     {
-       static List<Friend> _sortedList;
-
         public static List<Friend> ByName(this List<Friend> friends)
         {
             return friends.OrderBy(x => x.Name).ToList();
         }
         
-        public static List<Friend> ByOnlineStatus(List<Friend> friends)
+        public static List<Friend> ByOnlineStatus(this List<Friend> friends)
         {
             return friends.OrderBy(x => x.IsOnline).ToList();
         }
         
-        public static List<Friend> ByLastSeen(List<Friend> friends)
+        public static List<Friend> ByLastSeen(this List<Friend> friends)
         {
             return friends.OrderBy(x => x.LastSeen).ToList();
         }
         
-        public static List<Friend> ByLevel(List<Friend> friends)
+        public static List<Friend> ByLevel(this List<Friend> friends)
         {
             return friends.OrderBy(x => x.Level).ToList();
         }
